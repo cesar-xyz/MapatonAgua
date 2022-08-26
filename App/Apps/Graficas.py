201b92655bb88c8c4040d24ae9a5b2b2a287ad0c
Cesar Guillermo Vazquez Alvarez <cdgva@outlook.com>
2022-08-26 18:55:46 -0500

    Graficas

--- App/Apps/Graficas.py
+++ App/Apps/Graficas.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import base64
 import pandas as pd
 import plotly.express as px
 
+
 def app():
     st.title("Graficas")
+
+    st.image("Graficas/Comparativa Georreferencia.svg")
+    st.image("Graficas/% pozos por Tipo Concesión .svg")
+    st.image("Graficas/% pozos por USOS.svg")
+    st.image("Graficas/% Acuíferos por uso.svg")
